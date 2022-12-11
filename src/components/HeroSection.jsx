9a6860200fbd0c98bf655b50fa1c65408d4f79ed
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-11 23:53:52 +0545

    hero section ss completed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
       <div className="container grid grid-two-column">
         <div className="section-hero-data">
           <p className="hero-top-data">This is me</p>
-          <h1 className="hero-hading">ANIsh POKHAREL</h1>
+          <h1 className="hero-heading">ANIsh d POKHAREL</h1>
           <p className="hero-para">i m anish pokharel</p>
           <Button className="btn hire-me-btn">
             <NavLink to="/contact" />
@@ -35,6 +35,40 @@ const Wrapper = styled.section`
 
   .btn {
     max-width: 16rem;
+  }
+  .hero-top-data {
+    text-transform: uppercase;
+    font-weight: 500;
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme.colors.helper};
+  }
+  .hero-heading {
+    text-transform: uppercase;
+    font-size: 6.4rem;
+  }
+
+  .hero-para {
+    margin-top: 1.5rem;
+    margin-bottom: 3.4rem;
+    max-width: 60rem;
+  }
+  .section-hero-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  picture {
+    text-align: center;
+  }
+
+  .hero-img {
+    max-width: 80%;
+  }
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid {
+      gap: 7.2rem;
+    }
   }
 `;
 export default HeroSection;
