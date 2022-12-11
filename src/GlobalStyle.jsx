1961adb6b286ce80f4e7cd10a57e27edf5ab847d
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-12 00:02:52 +0545

    global css for  hero section

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -45,6 +45,18 @@ export const GlobalStyle = createGlobalStyle`
   li {
     list-style:none;
   }
+  .container {
+    max-width: 120rem;
+    margin: 0 auto;
+  }
+  .grid {
+    display: grid;
+    gap: 9rem;
+  }
+  .grid-two-column {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  
 
   
  `;
