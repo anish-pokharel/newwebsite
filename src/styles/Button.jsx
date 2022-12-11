881b718bf0b5d2ddc5590b4198107f3df8a5879a
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-11 23:42:30 +0545

    button

--- src/styles/Button.jsx
+++ src/styles/Button.jsx
@@ -1,3 +1,17 @@
 import styled from "styled-components";
 
-export const Button = styled.button``;
+export const Button = styled.button`
+  text-decoration: none;
+  max-width: auto;
+  background-color: rgb(98 84 243);
+  color: rgb(255 255 255);
+  padding: 1.4rem 2.4rem;
+  border: none;
+  text-transform: uppercase;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  -webkit-transition: all 0.3s ease 0s;
+  -moz-transition: all 0.3s ease 0s;
+  -o-transition: all 0.3s ease 0s;
+`;
