f7c956854e880cec0d998c653c3f7caed79ae724
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-13 23:50:46 +0545

    break today

---
+++ src/Break.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Break = () => {
+  return (
+    <>
+      <h1>hello today i took break from every thing have a nice day</h1>
+    </>
+  );
+};
+
+export default Break;
