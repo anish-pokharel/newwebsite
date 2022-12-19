618b7eacbf8d8dad728ec3690d11b93c2adfe1a0
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-20 00:17:27 +0545

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "../styles/Button";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { FaDiscord, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -20,6 +21,67 @@ const Footer = () => {
             </div>
           </div>
         </section>
+        {/* footer section */}
+
+        <footer>
+          <div className="container grid grid-four-column">
+            <div className="footer-about">
+              <h3>Anish POKHAREL</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+                facere!
+              </p>
+            </div>
+            <div className="footer-subscribe">
+              <h3>Subscribe to get important updates</h3>
+              <form action="#">
+                <input
+                  type="email"
+                  required
+                  autoComplete="off"
+                  placeholder="Email"
+                />
+                <input type="submit" value="Subscribe" />
+              </form>
+            </div>
+            <div className="footer-social">
+              <h3>Follows Us</h3>
+              <div className="footer-social--icons">
+                <div>
+                  <FaDiscord className="icons" />
+                </div>
+                <div>
+                  <FaInstagram className="icons" />
+                </div>
+                <div>
+                  <a
+                    href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                    target="_blank"
+                  >
+                    <FaYoutube className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="footer-contact">
+              <h3>Call Us</h3>
+              <h3>+91 12345678978</h3>
+            </div>
+          </div>
+
+          <div className="fotter-bottom--section">
+            <hr />
+            <div className="container grid grid-two-column">
+              <p>
+                @{new Date().getFullYear()} Anish Pokharel.All right Reserrved
+              </p>
+              <div>
+                <p>PROVACY POLICY</p>
+                <p>TERMS & CONDITIONS</p>
+              </div>
+            </div>
+          </div>
+        </footer>
       </Wrapper>
     </>
   );
