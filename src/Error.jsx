e7f748c6411ce42d86bfafa3f227261c49f66b4d
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-19 23:40:25 +0545

    Eroor

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper>
+      <img src="./images/error.svg" alt="" />
+    </Wrapper>
+  );
 };
-
+const Wrapper = styled.section``;
 export default Error;
