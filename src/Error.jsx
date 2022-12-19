969a3b60170994a98ee671ab64c988d4b2aa01b1
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-19 23:43:28 +0545

    eroor html complete

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import { Button } from "./styles/Button";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
     <Wrapper>
       <img src="./images/error.svg" alt="" />
+      <NavLink to="/">
+        <Button className="btn">Go Back</Button>
+      </NavLink>
     </Wrapper>
   );
 };
