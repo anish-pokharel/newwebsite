a8adf68fa55ba7e1481ea18cacd635315710060f
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-19 23:44:33 +0545

    css complete

--- src/Error.jsx
+++ src/Error.jsx
@@ -13,5 +13,15 @@ const Error = () => {
     </Wrapper>
   );
 };
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  padding: 9rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  .btn {
+    font-size: 1.8rem;
+    margin-top: 3rem;
+  }
+`;
 export default Error;
