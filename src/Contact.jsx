e7350c1aa586035cc59b4684c82d42cf43c0e4d2
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-19 21:18:43 +0545

    css styles

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -24,11 +24,43 @@ const Contact = () => {
             autoComplete="off"
             required
           />
+          <input
+            type="email"
+            name="Email"
+            placeholder="Email"
+            autoComplete="off"
+            required
+          />
+          <textarea name="message" id="" cols="30" rows="6"></textarea>
+          <input type="submit" value="send" />
         </div>
       </div>
     </Wrapper>
   );
 };
-const Wrapper = styled.section``;
-
-export default Contact;
+const Wrapper = styled.section`
+  padding: 9rem 0 5rem 0;
+  .container {
+    margin-top: 6rem;
+    text-align: center;
+    .contact-form {
+      max-width: 50rem;
+      margin: auto;
+      .contact-inputs {
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+        input[type="submit"] {
+          cursor: pointer;
+          transition: all 0.2s;
+          &:hover {
+            background-color: ${({ theme }) => theme.colors.white};
+            border: 1px solid ${({ theme }) => theme.colors.btn};
+            color: ${({ theme }) => theme.colors.btn};
+            transform: scale(0.9);
+          }
+        }
+      }
+    }
+  }
+`;
