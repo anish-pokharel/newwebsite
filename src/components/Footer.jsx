1454ea03bce1e2dc53b134c20d50584503f4f27c
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-28 23:12:28 +0545

    ooter simple design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -103,17 +103,36 @@ const Wrapper = styled.section`
   footer {
     padding: 14rem 0 9rem 0;
     background-color: ${({ theme }) => theme.colors.footer_bg};
-  }
-  h3 {
-    color: ${({ theme }) => theme.colors.hr};
-    margin-bottom: 2.4rem;
-  }
-  p {
-    color: ${({ theme }) => theme.colors.white};
-  }
-  .footer-social--icons {
-    display: flex;
-    gap: 2rem;
+    h3 {
+      color: ${({ theme }) => theme.colors.hr};
+      margin-bottom: 2.4rem;
+    }
+    p {
+      color: ${({ theme }) => theme.colors.white};
+    }
+    .footer-social--icons {
+      display: flex;
+      gap: 2rem;
+      div {
+        padding: 1rem;
+        border-radius: 50%;
+        border: 2px solid ${({ theme }) => theme.colors.white};
+        .icons {
+          color: ${({ theme }) => theme.colors.white};
+          font-size: 2.4rem;
+          position: relative;
+          cursor: pointer;
+        }
+      }
+    }
+    .footer-bottom--section {
+      padding-top: 9rem;
+      hr {
+        margin-bottom: 2rem;
+        color: ${({ theme }) => theme.colors.hr};
+        height: 0.1px;
+      }
+    }
   }
 `;
 export default Footer;
