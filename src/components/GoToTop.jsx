2ac6c4aaa0a9d67b68b7e435ad91183fcd9ec26c
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-29 23:25:41 +0545

    btn call

--- src/components/GoToTop.jsx
+++ src/components/GoToTop.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+const gotoBtn = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+};
+
 const GoToTop = () => {
   return (
     <div className="top-btn" onClick={gotoBtn}>
